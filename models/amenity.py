df029243d6866fc6ddb9ca20e5496bca6bb725c2
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-05-10 12:27:25 -0700

    class amenity

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class Amenity """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """class Amenity that inherits from BaseModel"""
+
+    name = ""
