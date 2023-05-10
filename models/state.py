d581ce2e5acec18c8d8d04c7aab2546e95977aff
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-05-10 12:17:15 -0700

    class state

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class State """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class State that inherits from BaseModel"""
+
+    name = ""
