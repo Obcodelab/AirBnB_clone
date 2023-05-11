1fb812d5fb87ddc4339d249b9317d4a5a5a418ab
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-05-11 00:43:41 -0700

    create instance

--- console.py
+++ console.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 """Entry point of the command interpreter"""
 import cmd
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """command interpreter"""
 
     prompt = '(hbnb) '
+    clss = ('BaseModel', 'User', 'State', 'City', 'Amenity', 'Place', 'Review')
 
     def do_EOF(self, arg):
         """exits the program"""
@@ -22,6 +31,17 @@ class HBNBCommand(cmd.Cmd):
         """Does not execute anything """
 
         pass
+
+    def do_create(self, arg):
+        """ Creates a new instance, saves it and prints the id"""
+
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.clss:
+            print("** class doesn't exist **")
+        else:
+            print(eval(arg)().id)
+            storage.save()
 
 
 if __name__ == '__main__':
