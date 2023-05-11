8dc47aa42191c93ace1f3264a6ac304b9bce9825
OB <gideonayomide75@gmail.com>
2023-05-11 14:39:01 +0100

    file storage tests

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Test for storage
+"""
+import os
+import unittest
+
+from models.engine.file_storage import FileStorage
+from models.user import User
+
+
+class test_fileStorage(unittest.TestCase):
+    """Test FileStorage Class"""
+
+    def test_instances(self):
+        """Testing instance of FileStorage Class"""
+        obj = FileStorage()
+        self.assertIsInstance(obj, FileStorage)
+
+    def test_all(self):
+        """
+        Tests method: all (returns dictionary <class>.<id> : <obj instance>)
+        """
+        storage = FileStorage()
+        instances_dic = storage.all()
+        self.assertIsNotNone(instances_dic)
+        self.assertEqual(type(instances_dic), dict)
+        self.assertIs(instances_dic, storage._FileStorage__objects)
+
+    def test_new(self):
+        """
+        Tests method: new (saves new object into dictionary)
+        """
+        m_storage = FileStorage()
+        instances_dic = m_storage.all()
+        melissa = User()
+        melissa.id = 999999
+        melissa.name = "Melissa"
+        m_storage.new(melissa)
+        key = melissa.__class__.__name__ + "." + str(melissa.id)
+        self.assertIsNotNone(instances_dic[key])
+
+    def test_reload(self):
+        """
+        Tests method: reload (reloads objects from string file)
+        """
+        a_storage = FileStorage()
+        try:
+            os.remove("file.json")
+        except:
+            pass
+        with open("file.json", "w") as f:
+            f.write("{}")
+        with open("file.json", "r") as r:
+            for line in r:
+                self.assertEqual(line, "{}")
+        self.assertIs(a_storage.reload(), None)
