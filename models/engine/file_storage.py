15e7946f26daab111e0f758df5a461e4d57f3c74
OB <gideonayomide75@gmail.com>
2023-05-11 09:37:33 +0100

    updated file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module for FileStorage class."""
+import datetime
 import json
 import os
 
@@ -7,6 +8,7 @@ import os
 class FileStorage:
 
     """Class for serialization and deserialization of base classes."""
+
     __file_path = "file.json"
     __objects = {}
 
@@ -22,9 +24,31 @@ class FileStorage:
     def save(self):
         """Serializes __objects to JSON file."""
         with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
-            data = {key: values.to_dict()
-                    for key, values in FileStorage.__objects.items()}
+            data = {
+                key: values.to_dict() for key, values in FileStorage.__objects.items()
+            }
             json.dump(data, file)
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references."""
+        from models.amenity import Amenity
+        from models.base_model import BaseModel
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+        from models.user import User
+
+        classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review,
+        }
+        return classes
 
     def reload(self):
         """Deserializes JSON file into __objects."""
@@ -32,6 +56,42 @@ class FileStorage:
             return
         with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
             obj_dict = json.load(file)
-            obj_dict = {key: self.classes()[values["__class__"]](**values)
-                        for key, values in obj_dict.items()}
+            obj_dict = {
+                key: self.classes()[values["__class__"]](**values)
+                for key, values in obj_dict.items()
+            }
             FileStorage.__objects = obj_dict
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname."""
+        attributes = {
+            "BaseModel": {
+                "id": str,
+                "created_at": datetime.datetime,
+                "updated_at": datetime.datetime,
+            },
+            "User": {
+                "email": str,
+                "password": str,
+                "first_name": str,
+                "last_name": str,
+            },
+            "State": {"name": str},
+            "City": {"state_id": str, "name": str},
+            "Amenity": {"name": str},
+            "Place": {
+                "city_id": str,
+                "user_id": str,
+                "name": str,
+                "description": str,
+                "number_rooms": int,
+                "number_bathrooms": int,
+                "max_guest": int,
+                "price_by_night": int,
+                "latitude": float,
+                "longitude": float,
+                "amenity_ids": list,
+            },
+            "Review": {"place_id": str, "user_id": str, "text": str},
+        }
+        return attributes
