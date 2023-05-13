35443aee114608b29f15c256f484aa2b4b38f693
OB <gideonayomide75@gmail.com>
2023-05-13 18:36:42 +0100

    updated console

--- console.py
+++ console.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """Entry point of the command interpreter"""
 import cmd
+
 from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
 from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
     """command interpreter"""
 
-    prompt = '(hbnb) '
-    clss = ('BaseModel', 'User', 'State', 'City', 'Amenity', 'Place', 'Review')
+    prompt = "(hbnb) "
+    clss = ("BaseModel", "User", "State", "City", "Amenity", "Place", "Review")
 
     def do_EOF(self, arg):
         """exits the program"""
@@ -29,12 +30,12 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
-        """Does not execute anything """
+        """Does not execute anything"""
 
         pass
 
     def do_create(self, arg):
-        """ Creates a new instance, saves it and prints the id"""
+        """Creates a new instance, saves it and prints the id"""
 
         if not arg:
             print("** class name missing **")
@@ -46,11 +47,11 @@ class HBNBCommand(cmd.Cmd):
             inst.save()
 
     def do_show(self, arg):
-        """ Prints the string representation of an instance
-            based on the class name and id
+        """Prints the string representation of an instance
+        based on the class name and id
         """
 
-        line = arg.split(' ')
+        line = arg.split(" ")
         ob_dict = storage.all()
         if not arg:
             print("** class name missing **")
@@ -67,11 +68,11 @@ class HBNBCommand(cmd.Cmd):
                 print(obj)
 
     def do_destroy(self, arg):
-        """ Deletes an instance based on the class name and id
-            (save the change into the JSON file)
+        """Deletes an instance based on the class name and id
+        (save the change into the JSON file)
         """
 
-        line = arg.split(' ')
+        line = arg.split(" ")
         obj_dict = storage.all()
         if not arg:
             print("** class name missing **")
@@ -88,9 +89,9 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
 
     def do_all(self, arg):
-        """ Prints all string representation of all instances
-            based or not on the class name.
-            The printed result must be a list of strings
+        """Prints all string representation of all instances
+        based or not on the class name.
+        The printed result must be a list of strings
         """
 
         list_obj = []
@@ -111,12 +112,12 @@ class HBNBCommand(cmd.Cmd):
             print(list_obj)
 
     def do_update(self, arg):
-        """ Updates an instance based on the class name and id
-            by adding or updating attribute
-            (save the change into the JSON file)
+        """Updates an instance based on the class name and id
+        by adding or updating attribute
+        (save the change into the JSON file)
         """
 
-        line = arg.split(' ')
+        line = arg.split(" ")
         if not arg:
             print("** class name missing **")
             return
@@ -154,33 +155,37 @@ class HBNBCommand(cmd.Cmd):
             return
 
     def default(self, arg):
-        """ Method called on an input line when the command prefix
-            is not recognized.
-            Retrieve all instances of a class by using: <class name>.all()
-            Retrieve the number of instances of a class: <class name>.count()
+        """Method called on an input line when the command prefix
+        is not recognized.
+        Retrieve all instances of a class by using: <class name>.all()
+        Retrieve the number of instances of a class: <class name>.count()
         """
 
         obj = storage.all()
-        line = arg.split('.')
+        line = arg.split(".")
         count = 0
 
-        if line[1] == 'all()':
+        if line[1] == "all()":
             self.do_all(line[0])
-        elif line[1] == 'count()':
+        elif line[1] == "count()":
             for i in obj:
-                key = i.split('.')
+                key = i.split(".")
                 if key[0] == line[0]:
                     count += 1
             print(count)
         elif line[1].startswith("show"):
             d_line = line[1].split('("')
-            d_line[1] = d_line[1].replace('")', '')
+            d_line[1] = d_line[1].replace('")', "")
             self.do_show(f"{line[0]} {d_line[1]}")
         elif line[1].startswith("destroy"):
             d_line = line[1].split('("')
-            d_line[1] = d_line[1].replace('")', '')
+            d_line[1] = d_line[1].replace('")', "")
             self.do_destroy(f"{line[0]} {d_line[1]}")
+        elif line[1].startswith("update"):
+            d_line = line[1].split('("')
+            d_line[1] = d_line[1].replace('")', "")
+            self.do_update(f"{line[0]} {d_line[1]}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
